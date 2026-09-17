e3007128cca5a9f7e739a4620db9638122821799
vipfamverify-ship-it <vipfamverify@gmail.com>
2026-09-17 11:26:48 +0530

    Update Youtube.py

--- VipMusic/platforms/Youtube.py
+++ VipMusic/platforms/Youtube.py
@@ -10,7 +10,7 @@ import aiohttp
 
 API_URL = "https://vipxofficial.in"
 
-API_KEY = "vipx6lOUvk4i1I8o2oeJGw0NdzfIP4rr" ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
+API_KEY = "vipxEyvgz7PZvT77O7PnGu7itFhY6wmy" ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
